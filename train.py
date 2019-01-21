7e0095c2906a743cbd1660213249456999cce618
alejodosr <alejandro.rramos@alumnos.upm.es>
2019-01-21 20:21:30 +0100

    bug fix

--- train.py
+++ train.py
@@ -265,7 +265,8 @@ def main(args=None):
             # Saving model back to drive
             print("Saving back to Drive.. model(" + parser.model_name + ")")
             drive_path = 'cp -rf snapshots/*' + ' /content/drive/My Drive/PhD/cloud/projects/' + parser.project_name + '/results/training/' + parser.model_name
-            commands.getstatusoutput(drive_path)
+            sts, text = commands.getstatusoutput(drive_path)
+            print(text)
 
         for iter_num, data in enumerate(dataloader_train):
             try:
